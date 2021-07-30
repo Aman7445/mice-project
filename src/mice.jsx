7ac69f2b364ae443bc08bf2148e9c0e6e53c48d3
Aman7445 <ak858696@gmail.com>
2021-07-30 19:56:30 +0530

    dataset increased

--- src/mice.jsx
+++ src/mice.jsx
@@ -1,20 +1,49 @@
-export const mice=[
-  { 
-    id:1,
-    Company:'Dell MS116',
-    Remarks: 'Great Aesthetics, Usage ideal for office, education sector,gaming,etc',
-    Image: 'https://m.media-amazon.com/images/I/51lOpzsvv2L._AC_UY327_FMwebp_QL65_.jpg'
+export const mice = [
+  {
+    id: 1,
+    Company: "Dell MS116",
+    Remarks:
+      "Great Aesthetics, Usage ideal for office, education sector,gaming,etc",
+    Image:
+      "https://m.media-amazon.com/images/I/51lOpzsvv2L._AC_UY327_FMwebp_QL65_.jpg",
   },
   {
-    id:2,
-    Company:'Lenovo Plug & Play USB Mouse',
-    Remarks: 'Comfortable to use for long hours with great performance',
-    Image: 'https://m.media-amazon.com/images/I/51Kh5lN0h3L._AC_UY327_FMwebp_QL65_.jpg'
+    id: 2,
+    Company: "Lenovo Plug & Play USB Mouse",
+    Remarks: "Comfortable to use for long hours with great performance",
+    Image:
+      "https://m.media-amazon.com/images/I/51Kh5lN0h3L._AC_UY327_FMwebp_QL65_.jpg",
   },
   {
-    id:3,
-    Company:'Corsair Katar Pro Ultra-Light Optical Gaming Mouse',
-    Remarks:'Ultra-Light and Agile:Weighing just 69g,great for hours of fast-paced FPS or MOBA gameplay',
-    Image:'https://m.media-amazon.com/images/I/81nEx+Nxw9L._AC_UY327_FMwebp_QL65_.jpg'
-  }
-]
+    id: 3,
+    Company: "Corsair Katar Pro Ultra-Light Optical Gaming Mouse",
+    Remarks:
+      "Ultra-Light and Agile:Weighing just 69g,great for hours of fast-paced FPS or MOBA gameplay",
+    Image:
+      "https://m.media-amazon.com/images/I/81nEx+Nxw9L._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    id: 4,
+    Company: "Logitech G102 Light Sync Gaming Mouse",
+    Remarks:
+      "Primary buttons are mechanical and tensioned with durable metal springs for reliability, performance and an excellent feel",
+    Image:
+      "https://m.media-amazon.com/images/I/61UxfXTUyvL._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    id: 5,
+    Company: "Logitech M90 Wired USB Mouse",
+    Remarks:
+      "Simple to set up and use ,no software to install, you can just plug the cable into a USB port and use it right away",
+    Image:
+      "https://m.media-amazon.com/images/I/61-xW5xl6-L._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    id: 6,
+    Company: "Corsair Glaive PRO RGB",
+    Remarks:
+      "Comfortable contoured shape: Naturally fits your hand so that you can stay in the game longer",
+    Image:
+      "https://m.media-amazon.com/images/I/51z2tJ6LN+L._AC_UY327_FMwebp_QL65_.jpg",
+  },
+];
